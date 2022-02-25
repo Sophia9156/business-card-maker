caab1340df484601acbaff031ee62259dfbe1518
Sophia9156 <suhyun9156@gmail.com>
2022-02-25 19:03:55 +0900

    add card and edit

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './editor.module.css';
+import CardEditForm from '../card_edit_form/CardEditForm';
 
-const Editor = (props) => (
+const Editor = ({cards}) => (
   <section className={styles.editor}>
     <h2 className={styles.title}>Card Maker</h2>
+    {
+      cards.map(card => <CardEditForm card={card} />)
+    }
   </section>
 );
 
--- src/components/preview/Preview.jsx
+++ src/components/preview/Preview.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Card from '../card/Card';
 import styles from './preview.module.css';
 
-const Preview = (props) => (
+const Preview = ({cards}) => (
   <section className={styles.preview}>
     <h2 className={styles.title}>Card Preview</h2>
+    <ul className={styles.cards}>
+    {
+      cards.map(card => <Card card={card} />)
+    }
+    </ul>
   </section>
 );
 
